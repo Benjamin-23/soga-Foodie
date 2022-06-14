1e7cabdc1914e03d5267215514c8d95cfabfd053
Benjamin-23 <kitongabenja34@gmail.com>
2022-06-14 16:31:26 +0300

    login details

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -27,27 +27,47 @@ const checkForm = () =>{
 </script> 
 
 <template>
-  <h1>{{ msg }}</h1>
-  <div class="row ">
-      <div class="col">
+<div class="flexed ">
+ 
+  <div class="flex-column">
+    <div class="primary-bg container">
+      <div class="flex-column">
         <label class="form-title">Enter your Email</label>
         <input type="email" v-model="formValue.email" placeholder="example@gmail.com" name="form-email" id="email_form">
-      </div> 
+      </div>
 
-        <div class="col">
+        <div class="flex-column">
           <label class="form-title">Enter your Password</label>
           <input type="password" v-model="formValue.password" placeholder="password" name="user-password" id="password_form" >
         </div>
+        
         <button class="btn btn-primary" @click="checkForm" > next step</button> 
        <p>{{step}}</p>
-      <div class="error bg-danger" v-if="!formValidate.isRequired">All input required</div>
-  </div>
-
+      <div class="error error-bg" v-if="!formValidate.isRequired">All input required</div>
+      </div>
+  </div> 
+  <div class="text-centered">
+    <p>{{ msg }}</p>
+    </div>
+  
+</div>
 </template>
 
 <style scoped>
  .btn{
    margin: 20px;
  }
-
+ .container{ 
+  width: 400px;
+ 
+ }
+label{ 
+  text-align:left ;
+  margin-top: 10px;
+}
+ .text-centered p {
+  text-align: center;
+  font-size: 30px;
+  font-weight: 800;
+}
 </style>
